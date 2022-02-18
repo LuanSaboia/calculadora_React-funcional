386e1e9e487f23e704054464b602c5d7cf33d809
Luan Saboia <luan.saboia@gmail.com>
2022-02-18 15:44:37 -0300

    calculadoraInicial_React
    desenvolvimento de calculadora básica utilizando: React Funcional, Hooks e Flux

---
+++ src/reducer/index.js
@@ -0,0 +1,87 @@
+import React, { useState } from "react";
+
+import useStore from "./somaReducer";
+
+function ReducerHook() {
+  const [numero, setNumero] = useState('');
+  const [segundoNumero, setSegundoNumero] = useState('');
+
+  const [ store, dispatch ] = useStore()
+
+  const somar = () => {
+    const numeroInt = parseInt(numero)
+    const segNumeroInt = parseInt(segundoNumero)
+    
+    dispatch({
+      type: 'SOMA',
+      payload: numeroInt + segNumeroInt
+    })
+  }
+
+  const subtrair = () => {
+    const numeroInt = parseInt(numero)
+    const segNumeroInt = parseInt(segundoNumero)
+    
+    dispatch({
+      type: 'SUBTRACAO',
+      payload: numeroInt - segNumeroInt
+    })
+  }
+
+  const multiplicar = () => {
+    const numeroInt = parseInt(numero)
+    const segNumeroInt = parseInt(segundoNumero)
+    
+    dispatch({
+      type: 'MULTIPLICACAO',
+      payload: numeroInt * segNumeroInt
+    })
+  }
+
+  const dividir = () => {
+    const numeroInt = parseInt(numero)
+    const segNumeroInt = parseInt(segundoNumero)
+    let payLoad = 0
+    if(segNumeroInt > 0){
+      payLoad = numeroInt / segNumeroInt
+    } else{
+      payLoad = 'não é possivel dividir por 0'
+    }
+    
+    dispatch({
+      type: 'DIVISAO',
+      payload: payLoad
+    })
+  }
+
+  return (
+    <div>
+      Número 1:
+      <br />
+      <input
+        type="text"
+        value={numero}
+        onChange={(e) => setNumero(e.target.value)}
+      />
+      <br />
+      Número 2:
+      <br />
+      <input
+        type="text"
+        value={segundoNumero}
+        onChange={(e) => setSegundoNumero(e.target.value)}
+      />
+      <br />
+      <button onClick={somar}>Somar</button><br />
+      <button onClick={subtrair}>Subtrair</button><br />
+      <button onClick={multiplicar}>Multiplicar</button><br />
+      <button onClick={dividir}>Dividir</button><br />
+      Resultado:
+      <br />
+      <input type="text" value={store.resultado} readOnly />
+      <br />
+    </div>
+  );
+}
+
+export default ReducerHook;
